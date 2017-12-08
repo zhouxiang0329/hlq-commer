f2019e18b01de3112d845047687f4d7caae8535c
zhouxiang <2047033272@qq.com>
2017-12-08 18:19:57 +0800

    修改为html

--- hlq-commer-common/src/main/java/com/commer/app/entity/User.java
+++ hlq-commer-common/src/main/java/com/commer/app/entity/User.java
@@ -4,7 +4,7 @@ import java.io.Serializable;
 import java.util.Date;
 
 public class User implements Serializable{
-	private Integer id;
+    private Integer id;
 
     private Long usernumber;
 
@@ -12,15 +12,7 @@ public class User implements Serializable{
 
     private String password;
 
-    private Long classid;
-
-    private Integer credit;
-
-    private Integer identity;
-
-    private String imageurl;
-
-    private Integer sex;
+    private String account;
 
     private Date createtime;
 
@@ -56,44 +48,12 @@ public class User implements Serializable{
         this.password = password == null ? null : password.trim();
     }
 
-    public Long getClassid() {
-        return classid;
+    public String getAccount() {
+        return account;
     }
 
-    public void setClassid(Long classid) {
-        this.classid = classid;
-    }
-
-    public Integer getCredit() {
-        return credit;
-    }
-
-    public void setCredit(Integer credit) {
-        this.credit = credit;
-    }
-
-    public Integer getIdentity() {
-        return identity;
-    }
-
-    public void setIdentity(Integer identity) {
-        this.identity = identity;
-    }
-
-    public String getImageurl() {
-        return imageurl;
-    }
-
-    public void setImageurl(String imageurl) {
-        this.imageurl = imageurl == null ? null : imageurl.trim();
-    }
-
-    public Integer getSex() {
-        return sex;
-    }
-
-    public void setSex(Integer sex) {
-        this.sex = sex;
+    public void setAccount(String account) {
+        this.account = account;
     }
 
     public Date getCreatetime() {
@@ -104,10 +64,14 @@ public class User implements Serializable{
         this.createtime = createtime;
     }
 
-	@Override
-	public String toString() {
-		return "User [id=" + id + ", usernumber=" + usernumber + ", username=" + username + ", password=" + password
-				+ ", classid=" + classid + ", credit=" + credit + ", identity=" + identity + ", imageurl=" + imageurl
-				+ ", sex=" + sex + ", createtime=" + createtime + "]";
-	}
+    @Override
+    public String toString() {
+        return "User{" +
+                "id=" + id +
+                ", usernumber=" + usernumber +
+                ", username='" + username + '\'' +
+                ", password='" + password + '\'' +
+                ", createtime=" + createtime +
+                '}';
+    }
 }
--- hlq-commer-shopWeb/src/main/java/com/commer/app/shopController/ShopController.java
+++ hlq-commer-shopWeb/src/main/java/com/commer/app/shopController/ShopController.java
@@ -3,6 +3,8 @@ package com.commer.app.shopController;
 import javax.annotation.Resource;
 import javax.servlet.http.HttpSession;
 
+import com.commer.app.entity.User;
+import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -10,22 +12,18 @@ import org.springframework.web.servlet.ModelAndView;
 
 import com.commer.app.UserService.UserService;
 
-@RestController 
-@RequestMapping("/shop") 
+@Controller
+@RequestMapping("/shop")
 public class ShopController {
-	
-	@Resource  
-    private UserService userService;
-	
-	@GetMapping("")
-    public ModelAndView login(HttpSession session) {
-		return new ModelAndView("shop_home");
-			
-	}
-	
-	@GetMapping("/index")
-    public ModelAndView ShowIndex() {
-		return new ModelAndView("shop_home");
-			
+
+	@Resource
+	private UserService userService;
+
+
+	@RequestMapping("/index")
+	public String index() {
+		System.out.print(userService.selectByPrimaryKey(1));
+
+		return "test";
 	}
 }
